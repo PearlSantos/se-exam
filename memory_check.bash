2170dfc7e8c05817aa0e23d96af9fe70c2c31992
PearlSantos <pearljensantos@gmail.com>
2017-05-21 17:05:52 +0800

    modified script after testing

--- memory_check.bash
+++ memory_check.bash
@@ -1,43 +1,45 @@
 #!/bin/bash
 
 #Change this
-usage() { echo "Usage: $0 [-c Critical Threshold] [-w Warning Threshold] [-e Email Address] where -c greater than -w" exit 1; }
+usage() { 
+	echo "Usage: $0 [-c Critical Threshold] [-w Warning Threshold] [-e Email Address] where -c greater than -w" 
+	exit 1; }
 
 #Reading input using getopts
 while getopts c:w:e: option
 do
- case "${option}"
- in
- c) CRITTHRESH=${OPTARG}
+	case "${option}"
+	in
+	c) CRITTHRESH=${OPTARG}
 	;;
- w) WARNTHRESH=${OPTARG}
+	w) WARNTHRESH=${OPTARG}
 	;;
- e) EADD=${OPTARG}
+	e) EADD=${OPTARG}
 	;;
- *) usage
+	*) usage
 	;;
- esac
+	esac
 done
 
-if [ -z "${CRITTHRESH}" ] || [ -z "${WARNTHRESH}" ] || [-z "${EADD}"]; then
+if [ -z "${CRITTHRESH}" ] || [ -z "${WARNTHRESH}" ] || [ -z "${EADD}" ]; then
     usage
 fi
 
 #checking if CRITTHRESH greater than WARNTHRESH
-if ["${CRITTHRESH}" -gt "${WARNTHRESH}"]
-then
-usage
+if ["${CRITTHRESH}" -lt "${WARNTHRESH}"]
+	then
+	usage
 fi
 
-USED_MEMORY=$( free | grep Mem: | awk '{ print $3/$2*100 }' )
+USED_MEMORY=$( free | grep Mem: | awk '{ print int($3/$2*100) }' )
 
-if ["${USED_MEMORY}" -ge "${CRITTHRESH}"]
+if [[ "${USED_MEMORY}" -ge "${CRITTHRESH}" ]]
 then
-exit 2
-elif ["${USED_MEMORY}" -ge "${WARNTHRESH}"] && ["${USED_MEMORY}" -lt "${CRITTHRESH}"]; then
-exit 1
+exit 2;
+elif [[ "${USED_MEMORY}" -ge "${WARNTHRESH}" ]] && [[ "${USED_MEMORY}" -lt "${CRITTHRESH}" ]]; then
+exit 1;
 else
-exit 0
+exit 0;
 fi
 
 
