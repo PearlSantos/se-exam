0900cdd71aef60f8a6e9ccbb3f65a9ac272ecfe5
PearlSantos <pearljensantos@gmail.com>
2017-05-21 14:26:16 +0800

    completed the script

--- memory_check.bash
+++ memory_check.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Change this
-usage() { echo "Usage: $0 [-c Critical Threshold] [-w Warning Threshold] [-e Email Address]" exit 1; }
+usage() { echo "Usage: $0 [-c Critical Threshold] [-w Warning Threshold] [-e Email Address] where -c greater than -w" exit 1; }
 
 #Reading input using getopts
 while getopts c:w:e: option
@@ -29,7 +29,15 @@ then
 usage
 fi
 
+USED_MEMORY=$( free | grep Mem: | awk '{ print $3/$2*100 }' )
 
-
+if ["${USED_MEMORY}" -ge "${CRITTHRESH}"]
+then
+exit 2
+elif ["${USED_MEMORY}" -ge "${WARNTHRESH}"] && ["${USED_MEMORY}" -lt "${CRITTHRESH}"]; then
+exit 1
+else
+exit 0
+fi
 
 
