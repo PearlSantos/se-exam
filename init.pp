962db816caa3e2d156a5214caabdbe01410bd260
PearlSantos <pearljensantos@gmail.com>
2017-05-25 19:14:33 +0800

    adding init.pp

---
+++ init.pp
@@ -0,0 +1,41 @@
+class puppet {
+
+  
+  package { 'vim': ensure => 'installed' }
+  package { 'curl': ensure => 'installed' }
+  package { 'git': ensure => 'installed' }
+
+
+  user { 'monitor':
+	name	=> 'monitor',
+	home	=> "/home/monitor",
+	shell	=> '/bin/bash/',
+  }
+  
+  exec{'retrieve_file':
+	command => "/usr/bin/wget -q https://raw.githubusercontent.com/PearlSantos/se-exam/master/memory_check.bash -O /home/monitor/scripts/memory_check.bash",
+	creates => "/home/monitor/scripts/memory_check.bash",
+	
+  }
+  
+  file { '/home/monitor/scripts/':
+	ensure => 'directory',
+	require => Exec["retrieve_file"],
+  }
+  
+  file { '/home/monitor/src/':
+	ensure => 'directory'
+  }
+  
+  file{ '/home/monitor/src/my_memory_check'
+	ensure => 'link',
+	target => '/home/monitor/scripts/memory_check.bash",
+  }
+  
+  cron{ 'my_memory_check':
+	command => '/home/monitor/src/my_memory_check',
+	minute => '*/10',
+  }
+	
+
+}
